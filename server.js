33dab56fa52d93b2abb5263c7a8be504ba88b3fe
kanoute021080 <kanoute021080@gmail.com>
2026-05-09 00:51:33 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type"]
+}));
+
 app.use(express.json());
 
 app.post("/chat", async (req, res) => {
